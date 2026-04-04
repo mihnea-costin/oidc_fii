458f0dd4fd904f8b7ad4b88f2ff90f8470f0c728
Mihnea-Radu Costin <79205548+mihnea-costin@users.noreply.github.com>
2026-04-04 12:31:19 +0000

    new

--- provider.tf
+++ provider.tf
@@ -17,7 +17,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "kdsjhfhdsohgiodshg6548"
+    bucket  = "bucket-mrc0304"
     key     = "terraform.tfstate"
     region  = "eu-north-1"
     encrypt = true
